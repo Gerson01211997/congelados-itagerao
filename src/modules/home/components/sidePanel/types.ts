1e982fa1471f06579ebe077185cf02108a4f4dbe
Gerson Hoyos <gersonhoyos_11@hotmail.com>
2026-02-12 15:23:33 -0500

    first commit

--- src/modules/home/components/sidePanel/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Transaction } from "@/services/transactions/transactions.types";
-
-export interface SidePanelProps {
-  isOpen: boolean;
-  onClose: () => void;
-  transaction: Transaction | null;
-}
